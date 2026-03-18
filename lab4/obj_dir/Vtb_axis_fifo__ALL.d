92041c9e3b0a43643043e6b2c46b3de668b8da8c
Gajula, Anish <agajula7@gatech.edu>
2026-03-17 21:36:54 -0400

    i have something that almost works

---
+++ lab4/obj_dir/Vtb_axis_fifo__ALL.d
@@ -0,0 +1,25 @@
+Vtb_axis_fifo__ALL.o: Vtb_axis_fifo__ALL.cpp Vtb_axis_fifo.cpp \
+  Vtb_axis_fifo.h /usr/local/share/verilator/include/verilated.h \
+  /usr/local/share/verilator/include/verilatedos.h \
+  /usr/local/share/verilator/include/verilated_config.h \
+  /usr/local/share/verilator/include/verilated_types.h \
+  /usr/local/share/verilator/include/verilated_funcs.h \
+  /usr/local/share/verilator/include/vltstd/svdpi.h \
+  Vtb_axis_fifo__Syms.h Vtb_axis_fifo___024root.h \
+  Vtb_axis_fifo_tb_axis_fifo.h \
+  /usr/local/share/verilator/include/verilated_vcd_c.h \
+  /usr/local/share/verilator/include/verilated_trace.h \
+  /usr/local/share/verilator/include/verilated_trace_defs.h \
+  /usr/local/share/verilator/include/verilated_dpi.h \
+  /usr/local/share/verilator/include/verilated_sym_props.h \
+  Vtb_axis_fifo___024root__DepSet_h1c3930a1__0.cpp \
+  Vtb_axis_fifo___024root__DepSet_h8801b18d__0.cpp \
+  Vtb_axis_fifo_tb_axis_fifo__DepSet_hac33bd97__0.cpp \
+  Vtb_axis_fifo_tb_axis_fifo__DepSet_h180b3e63__0.cpp \
+  Vtb_axis_fifo__Dpi.cpp Vtb_axis_fifo__Dpi.h \
+  Vtb_axis_fifo__Trace__0.cpp Vtb_axis_fifo___024root__Slow.cpp \
+  Vtb_axis_fifo___024root__DepSet_h1c3930a1__0__Slow.cpp \
+  Vtb_axis_fifo___024root__DepSet_h8801b18d__0__Slow.cpp \
+  Vtb_axis_fifo_tb_axis_fifo__Slow.cpp \
+  Vtb_axis_fifo_tb_axis_fifo__DepSet_h180b3e63__0__Slow.cpp \
+  Vtb_axis_fifo__Syms.cpp Vtb_axis_fifo__Trace__0__Slow.cpp
---
+++ lab4/obj_dir/test_axis_fifo.d
@@ -0,0 +1,12 @@
+test_axis_fifo.o: ../test_axis_fifo.cpp \
+  /usr/local/share/verilator/include/verilated.h \
+  /usr/local/share/verilator/include/verilatedos.h \
+  /usr/local/share/verilator/include/verilated_config.h \
+  /usr/local/share/verilator/include/verilated_types.h \
+  /usr/local/share/verilator/include/verilated_funcs.h Vtb_axis_fifo.h \
+  /usr/local/share/verilator/include/vltstd/svdpi.h \
+  Vtb_axis_fifo__Syms.h Vtb_axis_fifo___024root.h \
+  Vtb_axis_fifo_tb_axis_fifo.h \
+  /usr/local/share/verilator/include/verilated_vcd_c.h \
+  /usr/local/share/verilator/include/verilated_trace.h \
+  /usr/local/share/verilator/include/verilated_trace_defs.h
